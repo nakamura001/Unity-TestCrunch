565736acd76f6a95ca4f9fdfda713f9be8d51103
takashi nakamura <polluxtk@gmail.com>
2017-12-25 20:53:24 +0900

    Initial commit.

---
+++ Assets/Scripts/GraphicsDeviceType.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphicsDeviceType : MonoBehaviour {
+    public Text infoText;
+
+	void Start () {
+        infoText.text = SystemInfo.graphicsDeviceType.ToString();
+	}
+}
